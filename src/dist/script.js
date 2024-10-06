358fd977162ae1497f24b365825edcdd009ea19d
uuzor <victor.u.ezealor@gmail.com>
2024-10-06 16:45:28 +0100

    added hero

--- src/dist/script.js
+++ src/dist/script.js
@@ -1,9 +1,21 @@
-document.getElementById("menu").addEventListener("click", function(e){
+document.getElementById("menu").addEventListener("focusin", function(e){
     e.preventDefault();
-
+    console.log("focused");
+    
     var menubar = document.getElementById("menubar");
     menubar.classList.toggle("animateIn");
     menubar.classList.toggle("hidden");
     console.log("click");
     
 })
+
+document.getElementById("menu").addEventListener("focusout", (e)=> {
+
+    e.preventDefault();
+    var menubar = document.getElementById("menubar");
+    menubar.classList.toggle("animateIn");
+    menubar.classList.toggle("hidden");
+    console.log("focus out");
+})
+
+
